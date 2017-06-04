998cac8c1cc2dfb30145ca2949a9a32c49150dcf
alexanderfrei <whutmail@gmail.com>
2017-06-05 00:09:36 +0300

    async coroutines

--- python_snippets/threading and parallel cpu/coroutines.py
+++ python_snippets/threading and parallel cpu/coroutines.py
@@ -13,3 +13,11 @@ it = my_coroutine()
 next(it)  # start call is necessary to prepare generator
 for i in range(5):
     it.send(i)
+
+
+# TODO:
+""" modern python 3.5 way 
+https://habrahabr.ru/post/266743/
+https://docs.python.org/3/library/asyncio-task.html
+https://www.python.org/dev/peps/pep-0492/
+"""
