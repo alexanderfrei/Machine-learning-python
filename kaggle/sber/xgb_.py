6dc2679ddce2cb18a4bda24242ffb18d48ebf46d
Alexandre <whutmail@gmail.com>
2017-06-25 20:16:52 +0300

    xgb sber update

--- kaggle/sber/xgb_.py
+++ kaggle/sber/xgb_.py
@@ -1,190 +1,138 @@
-import math
-import numpy as np
-import pandas as pd
-import xgboost as xgb
-import matplotlib.pyplot as plt
-import os
+def main():
 
-os.chdir("d:/datasets/sber")
+    import math
+    import numpy as np
+    import pandas as pd
+    import xgboost as xgb
+    import matplotlib.pyplot as plt
+    import os
 
+    os.chdir("d:/datasets/sber")
 
-# From here: https://www.kaggle.com/robertoruiz/sberbank-russian-housing-market/dealing-with-multicollinearity/notebook
-macro_cols = ["balance_trade", "balance_trade_growth", "eurrub", "average_provision_of_build_contract",
-"micex_rgbi_tr", "micex_cbi_tr", "deposits_rate", "mortgage_value", "mortgage_rate",
-"income_per_cap", "rent_price_4+room_bus", "museum_visitis_per_100_cap", "apartment_build"]
+    # load data
+    df_train = pd.read_csv("train.csv", parse_dates=['timestamp'])
+    df_test = pd.read_csv("test.csv", parse_dates=['timestamp'])
+    df_macro = pd.read_csv("macro.csv", parse_dates=['timestamp'])
+    df_fix = pd.read_csv("BAD_ADDRESS_FIX.csv", sep=",")
 
-
-
-# load
-df_train = pd.read_csv("train.csv", parse_dates=['timestamp'])
-df_test = pd.read_csv("test.csv", parse_dates=['timestamp'])
-df_macro = pd.read_csv("macro.csv", parse_dates=['timestamp'], usecols=['timestamp'] + macro_cols)
-# df_macro = pd.read_csv("macro.csv", parse_dates=['timestamp'])
-
-id_test = df_test['id']
+    id_test = df_test['id']
+    id_train = df_train['id']
 
 
-# indexing
-df_fix = pd.read_csv("BAD_ADDRESS_FIX.csv", sep=",")
-df_all = pd.concat([df_train, df_test])
+    # fix
+    df_all = pd.concat([df_train, df_test])
 
-fix_cols = df_fix.columns[1:].values
-fix_ids = df_fix.iloc[:, 0]
+    fix_cols = df_fix.columns[1:].values
+    fix_ids = df_fix.iloc[:, 0]
 
-df_all.set_index('id', inplace=True)
-df_fix.set_index('id', inplace=True)
+    df_all.set_index('id', inplace=True)
+    df_fix.set_index('id', inplace=True)
 
+    def replace_col_value_by_ids(col, df_fix):
+        return df_fix.loc[:, col.name]
 
-
-# fix
-def replace_col_value_by_ids(col, df_fix):
-    return df_fix.loc[:, col.name]
-
-df_all.loc[fix_ids, fix_cols] = df_all.loc[fix_ids, fix_cols].apply(replace_col_value_by_ids, df_fix=df_fix)
+    df_all.loc[fix_ids, fix_cols] = df_all.loc[fix_ids, fix_cols].apply(replace_col_value_by_ids, df_fix=df_fix)
 
 
-
-# ylog_train_all = np.log1p(df_train['price_doc'].values)  # log(y + 1)
-ylog_train_all = df_train['price_doc']
-df_all.drop('price_doc', axis=1, inplace=True)  # drop id and y
-df_all = pd.merge_ordered(df_all, df_macro, on='timestamp', how='left')
-
-
-# Add month-year counts
-month_year = (df_all.timestamp.dt.month + df_all.timestamp.dt.year * 100)
-month_year_cnt_map = month_year.value_counts().to_dict()
-df_all['month_year_cnt'] = month_year.map(month_year_cnt_map)
-
-# Add week-year count
-week_year = (df_all.timestamp.dt.weekofyear + df_all.timestamp.dt.year * 100)
-week_year_cnt_map = week_year.value_counts().to_dict()
-df_all['week_year_cnt'] = week_year.map(week_year_cnt_map)
-
-# Add month and day-of-week
-df_all['month'] = df_all.timestamp.dt.month
-df_all['dow'] = df_all.timestamp.dt.dayofweek
-
-# Relative squares and floor
-df_all['rel_floor'] = df_all['floor'] / df_all['max_floor'].astype(float)
-df_all['rel_kitch_sq'] = df_all['kitch_sq'] / df_all['full_sq'].astype(float)
-
-# Remove timestamp column (may overfit the model in train)
-df_all.drop(['timestamp'], axis=1, inplace=True)
+    num_train = len(df_train)
+    y = np.log1p(df_train['price_doc'].values)  # log(y + 1)
+    df_all.drop('price_doc', axis=1, inplace=True)
+    df_all = pd.merge_ordered(df_all, df_macro, on='timestamp', how='left')
 
 
-# Deal with categorical values
+    # feature engineering
 
-df_numeric = df_all.select_dtypes(exclude=['object'])
-df_obj = df_all.select_dtypes(include=['object']).copy()
+    # Add month-year counts
+    month_year = (df_all.timestamp.dt.month + df_all.timestamp.dt.year * 100)
+    month_year_cnt_map = month_year.value_counts().to_dict()
+    df_all['month_year_cnt'] = month_year.map(month_year_cnt_map)
 
-for c in df_obj:
-    df_obj[c] = pd.factorize(df_obj[c])[0]
+    # Add week-year count
+    week_year = (df_all.timestamp.dt.weekofyear + df_all.timestamp.dt.year * 100)
+    week_year_cnt_map = week_year.value_counts().to_dict()
+    df_all['week_year_cnt'] = week_year.map(week_year_cnt_map)
 
-df_values = pd.concat([df_numeric, df_obj], axis=1)
+    # Add month and day-of-week
+    df_all['month'] = df_all.timestamp.dt.month
+    df_all['dow'] = df_all.timestamp.dt.dayofweek
 
+    # Relative squares and floor
+    df_all['rel_floor'] = df_all['floor'] / df_all['max_floor'].astype(float)
+    df_all['rel_kitch_sq'] = df_all['kitch_sq'] / df_all['full_sq'].astype(float)
 
-# Convert to numpy array
-X_all = df_values.values
+    # macro feature selection VIF
+    # From here: https://www.kaggle.com/robertoruiz/sberbank-russian-housing-market/dealing-with-multicollinearity/notebook
+    macro_vif = ["balance_trade", "balance_trade_growth", "eurrub", "average_provision_of_build_contract",
+    "micex_rgbi_tr", "micex_cbi_tr", "deposits_rate", "mortgage_value", "mortgage_rate",
+    "income_per_cap", "rent_price_4+room_bus", "museum_visitis_per_100_cap", "apartment_build"]
 
-# Create a validation set, with last 20% of data
-num_train = len(df_train)
-num_val = int(num_train * 0.2)
+    s = set(df_macro.columns)
+    macro_columns = list(s.difference(macro_vif))
+    df_all.drop(macro_columns, axis=1, inplace=True)
 
-X_train_all = X_all[:num_train]
-X_train = X_all[:num_train-num_val]
-X_val = X_all[num_train-num_val:num_train]
+    # df_all.drop(['timestamp'], axis=1, inplace=True)
 
-ylog_train = ylog_train_all[:-num_val]
-ylog_val = ylog_train_all[-num_val:]
+    # Deal with categorical values
+    df_numeric = df_all.select_dtypes(exclude=['object'])
+    df_obj = df_all.select_dtypes(include=['object']).copy()
 
-X_test = X_all[num_train:]
-df_columns = df_values.columns
+    for c in df_obj:
+        df_obj[c] = pd.factorize(df_obj[c])[0]
 
-
-
-# create xgboost objects
-dtrain_all = xgb.DMatrix(X_train_all, ylog_train_all, feature_names=df_columns)
-dtrain = xgb.DMatrix(X_train, ylog_train, feature_names=df_columns)
-dval = xgb.DMatrix(X_val, ylog_val, feature_names=df_columns)
-dtest = xgb.DMatrix(X_test, feature_names=df_columns)
-
-# grid
-
-eta = [0.05]
-max_depth = [4, 5, 6]
-subsample = [0.7]
-colsample_bytree = [0.7, 0.9]
-gamma = [0.0, 0.1, 0.3, 0.5]
-
-from itertools import product
-params_grid = product(eta, max_depth, subsample, colsample_bytree, gamma)
-
-default_params = {
-    'eta': 0.075,
-    'max_depth': 5,
-    'subsample': 1.0,
-    'colsample_bytree': 0.7,
-    'objective': 'reg:linear',
-    'eval_metric': 'rmse',
-    'silent': 1,
-    'gamma': 0,
-    'updater': 'grow_gpu'
-}
-
-watchlist  = [ (dtrain,'train'), (dval,'eval')]
-def rmsle_eval(preds, dtrain):
-    labels = dtrain.get_label()
-    return 'rmsle', np.sqrt(np.mean(np.power(np.log1p(preds)-np.log1p(labels), 2)))
-
-grid_result = []
-
-for i, params in enumerate(params_grid):
-
-    cache_params = default_params.copy()
-    cache_params['eta'] = params[0]
-    cache_params['max_depth'] = params[1]
-    cache_params['subsample'] = params[2]
-    cache_params['colsample_bytree'] = params[3]
-    cache_params['gamma'] = params[4]
-    cache_model = xgb.train(cache_params, dtrain, 2000, watchlist, feval=rmsle_eval,
-                            verbose_eval=50, early_stopping_rounds=20)
-    grid_result.append((params, cache_model.best_score))
-
-print(sorted(grid_result, key= lambda x: x[1])[:5])
+    df_values = pd.concat([df_numeric, df_obj], axis=1)
+    df_columns = df_values.columns
+    X_all = df_values.values
 
 
-# fit num boost rounds for best model
+    # train/validation split
 
-xgb_params = {
-    'eta': 0.05,
-    'max_depth': 5,
-    'subsample': 1.0,
-    'colsample_bytree': 0.7,
-    'silent': 1,
-    'objective': 'reg:linear',
-    'updater': 'grow_gpu'
-}
+    num_val = int(num_train * 0.2)
 
+    X_train_all = X_all[:num_train]
+    X_train = X_all[:num_train-num_val]
+    X_val = X_all[num_train-num_val:num_train]
+    y_train = y[:-num_val]
+    y_val = y[-num_val:]
 
-partial_model = xgb.train(xgb_params, dtrain, 1000,  watchlist, feval=rmsle_eval,
-                       early_stopping_rounds=20, verbose_eval=50)
+    X_test = X_all[num_train:]
 
 
-num_boost_round = partial_model.best_iteration  # assign best number of iterations
+    from xgboost import XGBRegressor
+    from sklearn.model_selection import GridSearchCV
 
-model = xgb.train(dict(xgb_params, silent=0), dtrain_all, num_boost_round=num_boost_round)  # fit model on all train data
-fig, ax = plt.subplots(1, 1, figsize=(8, 16))
-xgb.plot_importance(model, max_num_features=50, height=0.5, ax=ax)
+    default_params = {
+        'learning_rate': 0.075,
+        'max_depth': 5,
+        'subsample': 1.0,
+        'colsample_bytree': 0.7,
+        'objective': 'reg:linear',
+        'eval_metric': 'rmse',
+        'silent': 1,
+        'n_estimators': 1000
+    }
 
+    xgb_model = XGBRegressor(**default_params)
 
-# submission
-ylog_pred = model.predict(dtest)
-y_pred = np.exp(ylog_pred) - 1
+    fit_params = {
+        'eval_set': [(X_val, y_val)],
+        'early_stopping_rounds': 20,
+        'verbose': 50
+    }
 
-df_sub = pd.DataFrame({'id': id_test, 'price_doc': y_pred})
+    params = dict(learning_rate = [0.03, 0.05],
+                  max_depth = [5, 6],
+                  subsample = [0.9],
+                  colsample_bytree = [0.9],
+                  gamma = [0, 0.5])
 
-hash = ""
-if os.path.exists('sub.csv'): hash += str(np.random.randint(0, 10 ** 5 ) * np.random.randint(0, 10 ** 3 ) )
-df_sub.to_csv('sub{}.csv'.format(hash), index=False, )
+    grid_search = GridSearchCV(xgb_model, param_grid=params, cv=5, n_jobs=3, fit_params=fit_params)
+    grid_search.fit(X_train, y_train)
 
+    return grid_search
+
+if __name__ == "__main__":
+    dict = main()
+    with open('grid.txt', 'w') as f:
+        for k, v in dict.cv_results_.items():
+            f.writelines("{}: {}\n".format(k, v))
 
--- snippets/sklearn/preprocessing_.py
+++ snippets/sklearn/preprocessing_.py
@@ -1,5 +1,9 @@
 # LabelEncoder with pandas df
 
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+import numpy as np
+
 df = pd.DataFrame(np.random.randn(5, 3),
                   columns=['a', 'b', 'c'])
 
@@ -7,14 +11,9 @@ df['d'] = ['a', 'b', 'c', 'a', 'b']
 df['e'] = ['s', 'b', 'c', 'a', 'b']
 df2 = df = df.loc[:, ['d', 'e']]
 
-from sklearn.preprocessing import LabelEncoder
-from collections import defaultdict
-
 d = defaultdict(LabelEncoder)
 
 df = df.loc[:, ['d', 'e']]
 fit = df.apply(lambda x: d[x.name].fit_transform(x))  # Encoding the variable
 fit.apply(lambda x: d[x.name].inverse_transform(x))  # Inverse the encoded
 df2.apply(lambda x: d[x.name].fit_transform(x))  # use on new df
-
-
