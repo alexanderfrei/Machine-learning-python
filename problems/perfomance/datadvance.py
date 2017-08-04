79e133d78f7e166c98e4e0a9d68f8714bd672a98
alexanderfrei <whutmail@gmail.com>
2017-08-04 19:15:04 +0300

    perfomance

--- problems/perfomance/datadvance.py
+++ problems/perfomance/datadvance.py
@@ -12,27 +12,38 @@ def make_data(M, N):
     df.to_csv('incidents.csv', index_label='id')
 
 
-M, N = 2, 10 ** 4
+M, N = 2, 10 ** 5
 make_data(M, N)
 
 # work data
 
 df = pd.read_csv('incidents.csv')
 
-
 def similar_incidents(df, dt):
+
+    # vars
 
     arr = df.values
     col_1, col_2, col_3 = arr[:,1], arr[:,2], arr[:,3]
     cache=[]
 
-    def rowwise(row):
-        count_sim = len(np.where((col_1 == row[1]) & (col_2 == row[2]) & (col_3 >= row[3] - dt) & (col_3 <= row[3])\
-            [0])) - 1
+    # def rowwise(row):
+    #     count_sim = len(np.where((col_1 == row[1]) & (col_2 == row[2]) & (col_3 >= row[3] - dt) & (col_3 <= row[3])\
+    #         [0]))[0])) - 1
+    # along_axis(arr=arr, axis=1, func1d=rowwise)
+
+    for row in df.itertuples(index=False):
+        count_sim = len(np.where(
+            (col_1 == row[1]) & (col_2 == row[2]) & (col_3 >= row[3] - dt) & (col_3 < row[3]))[0])
         cache.append((row[0], count_sim))
 
-    np.apply_along_axis(arr=arr, axis=1, func1d=rowwise)
-    df = pd.DataFrame(cache, columns=['id','count'])
-    df.to_csv('incidents_count.csv', index_label='id')
+    # save file
 
-similar_incidents(df, 0.3)
+    df = pd.DataFrame(cache, columns=['id','count'])
+    df.to_csv('incidents_count.csv', index=False)
+
+
+if __name__ == "__main__":
+    similar_incidents(df, 1)
+
+
